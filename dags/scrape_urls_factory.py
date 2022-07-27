101e2eeb206b9883901554fc58e24030be5dd4c1
Wynne Lo <wynnemlo@gmail.com>
2022-07-27 19:29:34 +0800

    initial commit

---
+++ dags/scrape_urls_factory.py
@@ -0,0 +1,82 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+def create_dag(dag_id,
+               schedule,
+               dag_number,
+               default_args,
+               keyword,
+               salaryMin,
+               salaryMax):
+
+    def _scrape_job_urls(keyword: str, salaryMin: str, salaryMax: str, **kwargs):
+        import requests
+        from bs4 import BeautifulSoup
+        import re
+        import cloudscraper
+        import json
+        
+        # make request to JobsDB with specified params
+        scraper = cloudscraper.create_scraper()
+        url = f'https://hk.jobsdb.com/hk/search-jobs/{keyword}/1?SalaryF={salaryMin}&SalaryT={salaryMax}&SalaryType=1'
+        print('url: ', url)
+        try:
+            req = scraper.get(url)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+        # access left container
+        bs = BeautifulSoup(req.text, 'html.parser')
+        jobListContainer = bs.find('div', {'id': 'jobList'})
+        
+        # get number of pages
+        numOfPages = json.loads(jobListContainer['data-sol-meta'])['pageSize']
+        print('numOfPages: ', numOfPages)
+        
+        # loop through all pages and get all job links
+        print('Grabbing all job links in current page...')
+        jobList = jobListContainer.find_all('a', 
+            {'href': re.compile('\/hk\/en\/job.*')})
+        for job in jobList:
+            print(job['href'])
+    
+    dag = DAG(dag_id,
+              schedule_interval=schedule,
+              default_args=default_args,
+              tags=['scrape'])
+
+    with dag:
+        t1 = PythonOperator(
+            task_id='scrape_job_urls',
+            python_callable=_scrape_job_urls,
+            op_kwargs={'keyword': keyword,
+                       'salaryMin': salaryMin,
+                       'salaryMax': salaryMax})
+
+    return dag
+
+
+# build a dag for each number in range(10)
+salaryMin = [11000, 15000, 20000, 30000, 40000, 50000, 60000, 80000]
+salaryMax = [15000, 20000, 30000, 40000, 50000, 60000, 80000, 120000]
+keywords = ['data-analyst', 'data-scientist', 'analytics-manager', 'data-engineer', 'data-governance', 'data-steward', 'data-management', 'machine-learning']
+
+for keyword in keywords:
+    for n in range(len(salaryMin)):
+        dag_id = f'scrape_{keyword}_{salaryMin[n]}_{salaryMax[n]}'
+
+        default_args = {'owner': 'airflow',
+                        'start_date': datetime(2022, 7, 26),
+                        }
+
+        schedule = '@daily'
+        dag_number = n
+
+        globals()[dag_id] = create_dag(dag_id,
+                                    schedule,
+                                    dag_number,
+                                    default_args,
+                                    keyword,
+                                    salaryMin[n],
+                                    salaryMax[n])
---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region = var.region
+}
+
+resource "google_service_account" "default" {
+  account_id   = "airflow_instance"
+  display_name = "Service Account"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
